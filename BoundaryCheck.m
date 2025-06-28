0d08a532953a94df56e928053899f5ff65966323
AlexanderBazhenov <59746051+AlexanderBazhenov@users.noreply.github.com>
2025-06-28 22:14:01 +0300

    Add files via upload

---
+++ BoundaryCheck.m
@@ -0,0 +1,25 @@
+% 2025-06-27
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+##for ii = 1:length(tolpos0)
+##   [outinf, outsup]  = wedge(yint(ii), Yp(ii) );
+##   widwedge(ii) = outsup - outinf;
+##end
+##figure
+##plot(x0(tolpos0), widwedge, 'sk')
+
+for ii = 1:length(tolpos0)
+   dif1 = abs(inf(yint(ii))-inf(Yp(ii)));
+   dif2 = abs(sup(yint(ii))-sup(Yp(ii)));
+   dif3 = abs(inf(yint(ii))-sup(Yp(ii)));
+   dif4 = abs(sup(yint(ii))-inf(Yp(ii)));
+   Dif = [dif1, dif2, dif3, dif4];
+   DifY(ii) = min(Dif);
+end
+[DifYzero, DifYzeroInd] = find(DifY< 0.0001)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2025-06-28
+% Yp \subset yint
+for ii = 1 :length(DifYzeroInd)
+    [outinf, outsup] = wedge(yint(DifYzeroInd(ii)), Yp(DifYzeroInd(ii)));
+    BoundWid (ii) = (outsup - outinf) / (sup(Yp(DifYzeroInd(ii))) - inf(Yp(DifYzeroInd(ii))))
+end
--- RinvTollCorr.m
+++ RinvTollCorr.m
@@ -9,11 +9,254 @@
 ##  ytol0(ii) = argmax0(1) + argmax0(2)*X0(ii, 2);
 ##end
 ##%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CorriditINNxyCorr.m
 ##tolneg0 = find(env0(:,2)<0)
 ##tolpos0 = find(env0(:,2)>=0)
 ##eqnumber0 = env0(:,1)
 ##eqtol0 = env0(:,2)
+[eqnumber0sort, indeqnumber0sort] = sort(eqnumber0)
+eqtol0sort = eqtol0(indeqnumber0sort)
+tolneg0 = find(eqtol0sort < 0)
+tolpos0 = find(eqtol0sort >= 0)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+X = [ x0.^0 x0 ];
+Xpos = X ( tolpos0, :)
+y =  Rinnmid(tolpos0)
+epsilon = Rinnrad(tolpos0)
+
+lb = [-inf inf];                                # нижние границы beta1 и beta2
+irp_Rinv = ir_problem(Xpos, y, epsilon, lb);
 %
+[tolmaxpos0,argmaxpos0, envpos0] = tolsolvty(Xpos,Xpos,y-epsilon,y+epsilon,1);
+for ii=1:length(Xpos)
+  ytolmaxpos(ii) = argmaxpos0(1) + argmaxpos0(2)*Xpos(ii, 2)
+end
+%
+figure
+hold on
+%p1 = plot(eqnumber0, eqtol0,'sk')
+p1 = plot(x0(tolpos0),  Rinnmid(tolpos0),'sk')
+%p2 = plot(x0(tolneg0), Rinnmid(tolneg0),'sr')
+errorbar(x0(tolpos0),  Rinnmid(tolpos0),  Rinnrad(tolpos0),"~.b")
+%errorbar(x0(tolneg0), Rinnmid(tolneg0), Rinnrad(tolneg0), "~.r")
+p3 = plot(x0(tolpos0), ytolmaxpos, '--k')
+ lgd13 = legend([p1  p3], ...
+  {'Pos',  'argmaxpos'})
+  set(lgd13, 'fontsize', 14);
+ %   set(lgd12, 'location', 'northwest');
+set(gca, 'fontsize', 14)
+ylabel('Rinv')
+xlabel('BtIp')
+##xlim([xxBtIp(1) xxBtIp(end)])
+grid on
+titlestr = strcat('Rinv INN vs BtIp Pos')
+ht = title(titlestr)
+set(ht, 'fontweight', 'normal')
+ figure_name_out=strcat(titlestr, '.png')
+print('-dpng', '-r300', figure_name_out), pwd
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure
+hold on
+%p1 = plot(eqnumber0, eqtol0,'sk')
+p1 = plot(x0(tolpos0),  Rinnmid(tolpos0),'sk')
+p2 = plot(x0(tolneg0), Rinnmid(tolneg0),'sr')
+errorbar(x0(tolpos0),  Rinnmid(tolpos0),  Rinnrad(tolpos0),"~.b")
+errorbar(x0(tolneg0), Rinnmid(tolneg0), Rinnrad(tolneg0), "~.r")
+p3 = plot(x0(tolpos0), ytolmaxpos, '--k')
+ lgd123 = legend([p1 p2 p3], ...
+  {'Pos', 'Neg', 'argmaxpos'})
+  set(lgd123, 'fontsize', 14);
+ %   set(lgd12, 'location', 'northwest');
+set(gca, 'fontsize', 14)
+ylabel('Rinv')
+xlabel('BtIp')
+##xlim([xxBtIp(1) xxBtIp(end)])
+grid on
+titlestr = strcat('Rinv INN vs BtIp')
+ht = title(titlestr)
+set(ht, 'fontweight', 'normal')
+ figure_name_out=strcat(titlestr, '.png')
+print('-dpng', '-r300', figure_name_out), pwd
+
+
+figure
+hold on
+pcolor = [0.7 0.9 0.7]
+%xlimits = [0.0019 0.0056];
+% Points to predict
+xpfuture = [0.004 0.0045 0.005]'
+xp = [x0; xpfuture]
+Xp = [xp.^0 xp];
+%ir_plotmodelset(irp_steam, xlimits)
+yp = ir_predict(Xp, irp_Rinv)
+px = [ xp; xp(end:-1:1) ]
+py = [ yp(:, 1); yp(end:-1:1, 2) ]
+h2 =  patch(px,py,pcolor);
+  xlim([0.0018 0.0039])
+%
+errorbar(x0(tolpos0),  Rinnmid(tolpos0),  Rinnrad(tolpos0),"~.b")
+%errorbar(x0(tolneg0), Rinnmid(tolneg0), Rinnrad(tolneg0), "~.r")
+p1 = plot(x0(tolpos0),  Rinnmid(tolpos0),'sk')
+%p2 = plot(x0(tolneg0), Rinnmid(tolneg0),'sr')
+## lgd12 = legend([p1 p2 p3], ...
+##  {'Pos', 'Neg', 'argmax'})
+##  set(lgd123, 'fontsize', 14);
+%
+set(gca, 'fontsize', 14)
+ylabel('Rinv')
+xlabel('BtIp')
+##xlim([xxBtIp(1) xxBtIp(end)])
+grid on
+titlestr = strcat('Rinv INN vs BtIp Pos Predict')
+ht = title(titlestr)
+set(ht, 'fontweight', 'normal')
+ figure_name_out=strcat(titlestr, '.png')
+print('-dpng', '-r300', figure_name_out), pwd
+
+
+figure
+hold on
+pcolor = [0.7 0.9 0.7]
+%xlimits = [0.0019 0.0056];
+% Points to predict
+xpfuture = [0.004 0.0045 0.005]'
+xp = [x0(tolpos0); xpfuture]
+Xp = [xp.^0 xp];
+%ir_plotmodelset(irp_steam, xlimits)
+yp = ir_predict(Xp, irp_Rinv)
+px = [ xp; xp(end:-1:1) ]
+py = [ yp(:, 1); yp(end:-1:1, 2) ]
+h2 =  patch(px,py,pcolor);
+  xlim([0.0018 0.0039])
+
+% Forecast
+for ii = 1:length(tolpos0)
+  Yp(ii) = infsup(yp(ii,1), yp(ii,2));
+end
+% Data Interval
+yint = infsup(Rinnmid(tolpos0)-Rinnrad(tolpos0), Rinnmid(tolpos0)+Rinnrad(tolpos0))
+for ii = 1:length(x0)
+  ypmid(ii) = (yp(ii,1) + yp(ii,2))/2;
+end
+
+
+##for ii = 1:length(tolpos0)
+##   [outinf, outsup]  = wedge(yint(ii), Yp(ii) );
+##   widwedge(ii) = outsup - outinf;
+##end
+##figure
+##plot(x0(tolpos0), widwedge, 'sk')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%   BOUNDARY CHECK     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BoundaryCheck.m
+for ii = 1:length(tolpos0)
+   dif1 = abs(inf(yint(ii))-inf(Yp(ii)));
+   dif2 = abs(sup(yint(ii))-sup(Yp(ii)));
+   dif3 = abs(inf(yint(ii))-sup(Yp(ii)));
+   dif4 = abs(sup(yint(ii))-inf(Yp(ii)));
+   Dif = [dif1, dif2, dif3, dif4];
+   DifY(ii) = min(Dif);
+end
+[DifYzero, DifYzeroInd] = find(DifY< 0.0001)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+figure
+hold on
+pcolor = [0.7 0.9 0.7]
+%xlimits = [0.0019 0.0056];
+% Points to predict
+xpfuture = [0.004 0.0045 0.005]'
+xp = [x0; xpfuture]
+Xp = [xp.^0 xp];
+%ir_plotmodelset(irp_steam, xlimits)
+yp = ir_predict(Xp, irp_Rinv)
+px = [ xp; xp(end:-1:1) ]
+py = [ yp(:, 1); yp(end:-1:1, 2) ]
+h2 =  patch(px,py,pcolor);
+  xlim([0.0018 0.0039])
+%
+errorbar(x0(tolpos0),  Rinnmid(tolpos0),  Rinnrad(tolpos0),"~.k")
+%errorbar(x0(tolneg0), Rinnmid(tolneg0), Rinnrad(tolneg0), "~.r")
+p1 = plot(x0(tolpos0),  Rinnmid(tolpos0),'.k')
+%p2 = plot(x0(tolneg0), Rinnmid(tolneg0),'sr')
+## lgd12 = legend([p1 p2 p3], ...
+##  {'Pos', 'Neg', 'argmax'})
+##  set(lgd123, 'fontsize', 14);
+%
+errorbar(x0(tolpos0(DifYzeroInd)), Rinnmid(tolpos0(DifYzeroInd)), Rinnrad(tolpos0(DifYzeroInd)), "~.r")
+p2 = plot(x0(tolpos0(DifYzeroInd)), Rinnmid(tolpos0(DifYzeroInd)),'sr')
+%
+ lgd12 = legend([p1 p2 ], ...
+  {'Pos', 'Bouddary'})
+  set(lgd12, 'fontsize', 14);
+%
+grid on
+set(gca, 'fontsize', 14)
+ylabel('Rinv')
+xlabel('BtIp')
+##xlim([xxBtIp(1) xxBtIp(end)])
+grid on
+titlestr = strcat('Rinv INN vs BtIp Pos Predict Boundary')
+ht = title(titlestr)
+set(ht, 'fontweight', 'normal')
+ figure_name_out=strcat(titlestr, '.png')
+print('-dpng', '-r300', figure_name_out), pwd
+%%%%%%%%%%%%%%%%%%%%%%%%   /BOUNDARY CHECK     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%    RESIDUALS   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure
+hold on
+
+dataind = 1:length(x0);
+p0 = plot(xp(dataind),  Rinnmid(dataind)-ypmid(dataind)','.k')
+p1 = plot(xp(tolpos0),  Rinnmid(tolpos0)-ypmid(tolpos0)','sb')
+%p2 = plot(x0(tolneg0), Rinnmid(tolneg0),'sr')
+## lgd12 = legend([p1 p2 p3], ...
+##  {'Pos', 'Neg', 'argmax'})
+##  set(lgd123, 'fontsize', 14);
+%
+errorbar(xp(dataind),  Rinnmid(dataind)-ypmid(dataind)', Rinnrad(dataind), "~.k")
+p2 = plot(x0(tolpos0(DifYzeroInd)), Rinnmid(tolpos0(DifYzeroInd))-ypmid(tolpos0(DifYzeroInd))','sr')
+
+% Mode
+RinnResiduals = midrad(Rinnmid(dataind)-ypmid(dataind)', Rinnrad(dataind))
+ [mode, mu_array, max_mu, mode_ind, c_array, C, multi]= modeIR4(RinnResiduals)
+
+Colors_special
+figure
+hold on
+retval = ModePlot (mode, mu_array, max_mu, mode_ind, c_array, C, multi)
+titlestr = strcat('Interval mode', ' Residials INN')
+ht = title(titlestr)
+set(ht, 'fontweight', 'normal')
+figure_name_out=strcat(titlestr, '.png')
+print('-dpng', '-r300', figure_name_out), pwd
+
+%
+for ii = 1:length(dataind)
+  Res (ii)= (Rinnmid(ii)-ypmid(ii)')/ Rinnrad(ii);
+  Lev (ii) = 0.5*(-yp(ii,1) + yp(ii,2))/ Rinnrad(ii);
+end
+figure
+hold on
+plot(Lev, Res, '.k')
+grid on
+set(gca, 'fontsize', 14)
+xlabel('leverage')
+ylabel('res')
+titlestr = strcat(' res-leverage diagram', ' Residials INN')
+ht = title(titlestr)
+set(ht, 'fontweight', 'normal')
+figure_name_out=strcat(titlestr, '.png')
+print('-dpng', '-r300', figure_name_out), pwd
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%     INN EXTENSION  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 widadd = min(env0(tolpos0,2))
 envp = env0(tolpos0,2)
 step = 1
@@ -21,6 +264,8 @@ widadd = 0%envp(step)
 
 [eqnumber0sort, indeqnumber0sort] = sort(eqnumber0)
 eqtol0sort = eqtol0(indeqnumber0sort)
+tolneg0 = find(eqtol0sort < 0)
+tolpos0 = find(eqtol0sort >= 0)
 
 infR0 =    Rinnmid-Rinnrad;
 supR0 =    Rinnmid+Rinnrad;
